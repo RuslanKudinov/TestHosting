985406bff014d30dbdb89e85f0fcbc8488adceb7
Руслан Кудинов <r.kudinov@innopolis.university>
2026-01-29 02:17:14 +0300

    Fixed settings in Flask

--- RefFlask.py
+++ RefFlask.py
@@ -1,35 +1,17 @@
 from flask import Flask
-app = Flask(__name__) #application#main
+import os
 
-@app.route("/") # href - hyper reference
-def hello_world():# h - header, ul - unordered list, li - list item
-   return """
- <h1>Добро пожаловать на мой сайт!</h1>
- <ul>
-     <li><a href="/about">Первая страница</a></li>
-     <li><a href="/user">Вторая страница</a></li>
- </ul>
- """
+app = Flask(__name__)
 
-@app.route("/about")
-def about():
-    return """
- <h1>Первая страница</h1>
- <ul>
-     <li><a href="/">Домашняя страница</a></li>
- </ul>
- """
-@app.route("/user")
-def contact():
-    return """
- <h1>Вторая страница</h1>
- <ul>
-     <li><a href="/">Домашняя страница</a></li>
- </ul>
- """
+@app.route('/')
+def home():
+    return "Работает!"
 
-app.run(debug=True)
+# УБЕРИ ЭТО или закомментируй:
+# if __name__ == '__main__':
+#     app.run(debug=True)  # <-- ЭТО ПРОБЛЕМА!
 
-
-
-
+# ВМЕСТО ЭТОГО добавь в самый конец:
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False важно!
